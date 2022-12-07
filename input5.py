96ef209f5fb470cae61ed101a5cff79847c8dc0b
lsito <leonardo.sito@cern.ch>
2022-12-07 14:12:43 +0100

    Example file

--- input5.py
+++ input5.py
@@ -1,30 +1,33 @@
+# Example: using an LHC fill number to obtain beam parameters
+import bihc
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
 # Beam new is the new version of Francesco's library. It has been modified.
-from beam_new import Beam, Impedance
+# from beam_new import Beam, Impedance
 
 mpl.style.use(['ggplot'])
 
 # Data retrival from timber
-b_6675 = Beam(fillNumber=6675, bunchShape='BINOMIAL')
+b_6675 = bihc.Beam(fillNumber=6675, bunchShape='BINOMIAL')
 #b_6675=Beam( M=1320, bunchLength=4*0.05/3e8, d = 25e-9, phi=0, realMachineLength=True, Nb=0.6e11, bunchShape='GAUSSIAN', beamNumber=1, fillMode='FLATTOP', machine='LHC')
 [f,S] = b_6675.spectrum
 
 # Impedance plot
-impedance_file = 'ImpedanceDFT_NoPort.txt'
+impedance_file = 'Impedance_file.txt'
 
-Z = Impedance(f)
+Z = bihc.Impedance(f)
 Z.getImpedanceFromCST(impedance_file)
-Z.plot(fMax=Z.df.f.max())
+Z.plotImpedance(fMax=Z.f.max())
 
 # Spectrum plot 
 [f,S] = b_6675.spectrum
 plt.figure()
 plt.plot(f/1e9,S, label='Spectrum')
-plt.plot(Z.df.f/1e9, Z.df.Zr/Z.df.Zr.max(), label='Impedance Normalized')
+plt.plot(Z.f/1e9, Z.Zr/Z.Zr.max(), label='Impedance Normalized')
 
 
 # plt.plot(Zt.df.f/1e9, Zt.df.Zr/Zt.df.Zr.max(), label='FLATBOTTOM')
