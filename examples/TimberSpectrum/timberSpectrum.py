048d3442355fbe8bba03fb90c5b2d728b35bdd90
ELENA DE LA FUENTE <elena.delafuenteg@gmail.com>
2023-05-04 11:38:23 +0200

    spectrum setter

--- examples/TimberSpectrum/timberSpectrum.py
+++ examples/TimberSpectrum/timberSpectrum.py
@@ -49,23 +49,23 @@ def fillingSchemeLHC():
 beam = bihc.Beam(Np=2.3e11, bunchLength=1.2e-9, fillingScheme=fillingSchemeLHC(), machine='LHC', spectrum='user') 
 
 # Importing spectrum from timber
-print('Accessing timber...')
+print('Accessing Timber...')
 db=pytimber.LoggingDB() 
 time = pytimber.parsedate('2015-08-07 19:55:00')
 data = db.get('ALB.SR4.B1:SPEC_POW', time)['ALB.SR4.B1:SPEC_POW'][1][0] #in dB
-S = 10.0**(data/20)
+
+# Set spectrum for beam
+S = np.sqrt(10.0**((data-np.max(data))/10)) 
+#S = S/np.max(S)
 f = np.arange(0,len(S))*beam.frev
 spectrum = [f, S]
-beam.setSpectrum(spectrum) #???
+beam.setSpectrum(spectrum) 
 
-
-plt.plot(spectrum[0], spectrum[1])
+plt.plot(beam.spectrum[0], beam.spectrum[1])
 plt.show()
 
-'''
-# Importing an impedance curve
+# Obtain dissipated power from impedance file
 impedance_file = 'PillboxImpedance.txt'
-
 Z = bihc.Impedance()
 Z.getImpedanceFromCST(impedance_file)
 
@@ -74,4 +74,3 @@ beam.plotSpectrumAndImpedance(Z)
 
 # Computing the dissipated power value
 print(f'Beam 6675 power loss: {beam.getPloss(Z)[0]} W')
-'''
