6107a7c790f72762cb9db10be04e1b7784c83df8
ELENA DE LA FUENTE <elena.delafuenteg@gmail.com>
2023-11-13 18:27:57 +0100

    version update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="bihc",
-    version="0.0.8.5",
+    version="0.0.8.6",
     description="BIHC: Beam-Induced Heating Computation package",
     author="Francesco Giordano",
     author_email="benoit.salvant@cern.ch", 
