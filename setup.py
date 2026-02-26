82aa488068db0e131ebc66d10b96d3bbb3b760a0
elenafuengar <elena.delafuenteg@gmail.com>
2026-02-26 15:15:44 +0100

    :heavy_plus_sign: added tqdm as dependency

--- setup.py
+++ setup.py
@@ -3,18 +3,21 @@
 # Copyright (c) CERN, 2024.                   #
 # ########################################### #
 
-from setuptools import setup, find_packages
 from pathlib import Path
 
+from setuptools import find_packages, setup
+
 # read version
-version_file = Path(__file__).parent / 'bihc/_version.py'
+version_file = Path(__file__).parent / "bihc/_version.py"
 dd = {}
-with open(version_file.absolute(), 'r') as fp:
+with open(version_file.absolute(), "r") as fp:
     exec(fp.read(), dd)
-__version__ = dd['__version__']
+__version__ = dd["__version__"]
 
 # read long_description
-long_description = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
+long_description = (Path(__file__).parent / "README.md").read_text(
+    encoding="utf-8"
+)
 
 #########
 # Setup #
@@ -25,28 +28,28 @@ setup(
     version=__version__,
     description="BIHC: Beam-Induced Heating Computation package",
     author="Elena de la Fuente, Francesco Giordano, Leonardo Sito",
-    author_email="elena.de.la.fuente.garcia@cern.ch", 
+    author_email="elena.de.la.fuente.garcia@cern.ch",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     url="https://github.com/ImpedanCEI/BIHC",
     project_urls={"Bug Tracker": "https://github.com/ImpedanCEI/BIHC/issues"},
-    install_requires = [
-                    'numpy',
-                    'matplotlib',
-                    'scipy',
-                       ],
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scipy",
+        "tqdm",
+    ],
     classifiers=[
         "Development Status :: 4 - Beta",
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
         "Intended Audience :: Science/Research",
-		"Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Physics",
     ],
-
 )
